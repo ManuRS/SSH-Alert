8cb8a0e94a8f8e8bae18843ad884c209777e6505
Manuel Reyes <mnrs94@gmail.com>
2017-02-17 18:30:14 +0100

    Modo de reporte diario completado

--- dayReport.py
+++ dayReport.py
@@ -1,18 +1,41 @@
 import time
-import datetime
+import datetime as dt
+import smtplib
+import aux
+
+########
+# PARSEO
+########
 
 f = open('/var/log/auth.log', 'r')
 #print (f.read())
 
 text=""
 for line in f:
- if "password" in line:
-  if str(int(time.strftime("%d"))-1) in line:
-   if mydate.strftime("%B")[0:3]) in line:
+ if "password" in line or "AllowUsers" in line:
+  if " "+str(int(time.strftime("%d"))-1)+" " in line:
+   if dt.datetime.now().strftime("%B")[0:3] in line:
     text += line
-   
-print (text)
+    
+#print (text)
+    
+########
+# ENVIO
+########
 
-mydate = datetime.datetime.now()
-print (mydate.strftime("%B")[0:3])
+msg = "\r\n".join([
+  "From: " + aux.fromaddr,
+  "To: " + aux.toaddrs,
+  "Subject: SSH-Alert: "+ time.strftime("%d/%m/%Y"),
+  "",
+  text
+  ])
+  
+server = smtplib.SMTP('smtp.gmail.com:587')
+server.ehlo()
+server.starttls()
+server.login(aux.fromaddr,aux.pas)
+server.sendmail(aux.fromaddr, aux.toaddrs, msg)
+server.quit()
+   
 
